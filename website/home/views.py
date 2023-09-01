0eaee5efb7faa1ad96440df2daa18bc2863a644b
MarnoShepherd <marnoshepherd19@gmail.com>
2023-09-01 16:07:52 +0200

    Added docstrings

--- website/home/views.py
+++ website/home/views.py
@@ -2,7 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    """
+    Render the home page.
+
+    This view renders the 'home.html' template, which serves as the home page of
+    the website.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response rendering the 'home.html' template.
+    """
     return render(request, "home.html")
 
 def about(request):
+    """
+    Render the about page.
+
+    This view renders the 'about.html' template, which provides information about
+    the website or organization.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response rendering the 'about.html' template.
+    """
     return render(request, 'about.html')
